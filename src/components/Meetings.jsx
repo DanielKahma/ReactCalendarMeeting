2f02d5dba1b271eba895a9e5371407aefaadf7e4
Daniel Kahma <Daniel.kahma@hotmail.com>
2025-03-26 10:52:15 +0100

    did some more changes

---
+++ src/components/Meetings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MeetingForm from "./MeetingForm";
+import MeetingTable from "./MeetingTable";
+
+const Meetings = () => {
+  return (
+    <div>
+      <h1>Meetings</h1>
+      <MeetingForm />
+      <MeetingTable />
+    </div>
+  );
+};
